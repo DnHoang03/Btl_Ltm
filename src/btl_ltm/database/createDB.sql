60e9a4f3033e3cbd2422bd1dc5e5e2fb774cc31a
sinatranguyen <kiennv.contact@gmail.com>
2024-11-07 08:12:47 +0700

    feat: add create db script

---
+++ src/btl_ltm/database/createDB.sql
@@ -0,0 +1,7 @@
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    pass VARCHAR(255) NOT NULL,
+    score INT DEFAULT 0,
+    match_total INT DEFAULT 0
+);
